e4f6baeca5ecae60503c675f93272a058eaeb609
saharkamil <143878336+saharkamil@users.noreply.github.com>
2025-03-16 12:09:15 +0400

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const path = require('path');
 //      }));
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname)));
 app.post("/api/riskCaculator", (req, res) => {
     const {age, bmi, bp, disease} = req.body; 
     let point=0;
